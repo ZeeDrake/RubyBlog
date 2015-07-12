d960e9e5bc72289ccbfd377b9b4f61b55a33c050
ZeeDrake <ZeeDrake3000@gmail.com>
2015-07-11 22:09:48 -0500

    Added posts webpages to website

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def destory
+  end
+
+  def update  	
+  end
+
+  def create  	
+  end
+end
