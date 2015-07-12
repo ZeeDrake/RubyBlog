8b50a5ade9ccd29c77a8d8bbee5427e8712e8452
ZeeDrake <ZeeDrake3000@gmail.com>
2015-07-12 16:05:01 -0500

    Added associations

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :posts
+  resources :categories
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
